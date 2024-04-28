ad3d2eb4e0dfec4b43f5f6a7a648770b714d7790
Jair Cavalcante <jaircavalcante@MacBook-Pro-de-Jair.local>
2024-04-28 00:22:00 -0300

    feature: remove step 0 pipeline and test with problem

--- deploy.sh
+++ deploy.sh
@@ -4,32 +4,32 @@
 # Created database with dynamodb
 #############################################
 # shellcheck disable=SC1101
-aws dynamodb create-table \
-    --table-name clients \
-    --attribute-definitions AttributeName=cpf,AttributeType=S \
-    --key-schema AttributeName=cpf,KeyType=HASH \
-    --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
-    --endpoint-url ${AWS_DYNAMODB_ENDPOINT}
-    --region ${AWS_REGION}
-
-echo "Created table in Dynamodb completed!"
-
-#############################################
-# Build and push Docker image to DockerHub
-#############################################
-
-docker build -t tech-challenge-clients:v1.0 .
-docker push jaircmendes/tech-challenge-clients:v1.0
-
-
-#############################################
-# Deploy infrastructure using Terraform
-#############################################
-
-cd infra
-terraform init
-terraform validate
-terraform apply -auto-approve -var "aws_access_key=${AWS_ACCESS_KEY}" -var "aws_secret_key=${AWS_SECRET_KEY}"
-cd ..
-
+#aws dynamodb create-table \
+#    --table-name clients \
+#    --attribute-definitions AttributeName=cpf,AttributeType=S \
+#    --key-schema AttributeName=cpf,KeyType=HASH \
+#    --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
+#    --endpoint-url ${AWS_DYNAMODB_ENDPOINT}
+#    --region ${AWS_REGION}
+#
+#echo "Created table in Dynamodb completed!"
+#
+##############################################
+## Build and push Docker image to DockerHub
+##############################################
+#
+#docker build -t tech-challenge-clients:v1.0 .
+#docker push jaircmendes/tech-challenge-clients:v1.0
+#
+#
+##############################################
+## Deploy infrastructure using Terraform
+##############################################
+#
+#cd infra
+#terraform init
+#terraform validate
+#terraform apply -auto-approve -var "aws_access_key=${AWS_ACCESS_KEY}" -var "aws_secret_key=${AWS_SECRET_KEY}"
+#cd ..
+#
 echo "Deployment completed!"
