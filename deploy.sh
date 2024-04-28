c85a015a26a84e152be7c334fff717cbc5d43d8b
Jair Cavalcante <jaircavalcante@MacBook-Pro-de-Jair.local>
2024-04-28 00:08:04 -0300

    feature: add field about secret github

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ aws dynamodb create-table \
     --attribute-definitions AttributeName=cpf,AttributeType=S \
     --key-schema AttributeName=cpf,KeyType=HASH \
     --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
-    --endpoint-url dynamodb.${AWS_REGION}.amazonaws.com
+    --endpoint-url ${AWS_DYNAMODB_ENDPOINT}
     --region ${AWS_REGION}
 
 echo "Created table in Dynamodb completed!"
