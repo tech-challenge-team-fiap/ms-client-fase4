914296f2422d1d50f286137b38c65750fd6ee06d
Jair Cavalcante <jaircavalcante@MacBook-Pro-de-Jair.local>
2024-04-28 00:05:59 -0300

    feature: configuration file docker, dynamodb with script

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#############################################
+# Created database with dynamodb
+#############################################
+# shellcheck disable=SC1101
+aws dynamodb create-table \
+    --table-name clients \
+    --attribute-definitions AttributeName=cpf,AttributeType=S \
+    --key-schema AttributeName=cpf,KeyType=HASH \
+    --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
+    --endpoint-url dynamodb.${AWS_REGION}.amazonaws.com
+    --region ${AWS_REGION}
+
+echo "Created table in Dynamodb completed!"
+
+#############################################
+# Build and push Docker image to DockerHub
+#############################################
+
+docker build -t tech-challenge-clients:v1.0 .
+docker push jaircmendes/tech-challenge-clients:v1.0
+
+
+#############################################
+# Deploy infrastructure using Terraform
+#############################################
+
+cd infra
+terraform init
+terraform validate
+terraform apply -auto-approve -var "aws_access_key=${AWS_ACCESS_KEY}" -var "aws_secret_key=${AWS_SECRET_KEY}"
+cd ..
+
+echo "Deployment completed!"
