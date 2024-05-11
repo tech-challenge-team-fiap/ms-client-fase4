14f69cc4cbcaabd072efca39a8c53a9038296766
jaircavalcante <jcavalcante49@gmail.com>
2024-05-11 20:46:14 -0300

    include dependency files execution

--- infra/main.tf
+++ infra/main.tf
@@ -9,14 +9,9 @@ terraform {
   required_version = ">= 1.0.0"
 }
 
-# Data source para obter o account ID atual, usado na URL do container
 data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
 
-provider "aws" {
-  region = "us-east-2"
-}
-
-# Attach Policies to IAM User
 resource "aws_iam_user_policy_attachment" "ecr_policy_attachment" {
   user       = "jairmendes-dev"
   policy_arn = "arn:aws:iam::851725557582:policy/ECRCreateRepositoryPolicy"
@@ -32,24 +27,22 @@ resource "aws_iam_user_policy_attachment" "ec2_policy_attachment" {
   policy_arn = "arn:aws:iam::851725557582:policy/EC2CreateVpcPolicy"
 }
 
-# Create ECS Cluster Fargate
 resource "aws_ecs_cluster" "tc_ms_client_cluster" {
   name = "tc-ms-client-cluster"
 }
 
-# ECS Task Definition
 resource "aws_ecs_task_definition" "tc_ms_client_cluster_task" {
   family                   = "tc-ms-client-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  execution_role_arn       = aws_iam_user_policy_attachment.ec2_policy_attachment.policy_arn
-  cpu                      = "512" # 0.5 vCPU
-  memory                   = "1024" # 1GB
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  cpu                      = "512"
+  memory                   = "1024"
 
   container_definitions = jsonencode([
     {
       name      = "ts-ms-client-container"
-      image     = "${aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/tc-ms-client:latest"
+      image     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com/tc-ms-client:latest"
       essential = true
       portMappings = [
         {
@@ -67,7 +60,6 @@ resource "aws_ecs_task_definition" "tc_ms_client_cluster_task" {
   ])
 }
 
-# Criar a IAM Role para a Task Execution
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "ecs_task_execution_role"
 
@@ -85,17 +77,16 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   })
 }
 
-# 3. Criar Security Group para o cluster
 resource "aws_security_group" "tc_ms_client_sg" {
   name        = "tc-ms-client-sg"
   description = "Allow all inbound traffic"
-  vpc_id      = "vpc-025dcd22cfaa4c66a"
+  vpc_id      = "vpc-025dcd22cfaa4c66a"  # Consider dynamic retrieval
 
   ingress {
     description      = "Allow all inbound traffic"
     from_port        = 0
     to_port          = 0
-    protocol         = "-1" # All
+    protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
@@ -103,8 +94,7 @@ resource "aws_security_group" "tc_ms_client_sg" {
     description      = "Allow all outbound traffic"
     from_port        = 0
     to_port          = 0
-    protocol         = "-1" # All
+    protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
   }
 }
-
