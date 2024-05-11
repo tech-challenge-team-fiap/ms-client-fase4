4d48332dc93eb5117a2ce91dcdb84ab1ddcfdfa4
jaircavalcante <jcavalcante49@gmail.com>
2024-05-11 20:37:15 -0300

    ajuste arquivo docker-build-ecr e deploy-aws

--- infra/main.tf
+++ infra/main.tf
@@ -9,6 +9,9 @@ terraform {
   required_version = ">= 1.0.0"
 }
 
+# Data source para obter o account ID atual, usado na URL do container
+data "aws_caller_identity" "current" {}
+
 provider "aws" {
   region = "us-east-2"
 }
@@ -29,50 +32,79 @@ resource "aws_iam_user_policy_attachment" "ec2_policy_attachment" {
   policy_arn = "arn:aws:iam::851725557582:policy/EC2CreateVpcPolicy"
 }
 
-# ECS Cluster
-resource "aws_ecs_cluster" "tech_challenge_cluster" {
-  name = "tech_challenge_cluster"
+# Create ECS Cluster Fargate
+resource "aws_ecs_cluster" "tc_ms_client_cluster" {
+  name = "tc-ms-client-cluster"
 }
 
 # ECS Task Definition
-resource "aws_ecs_task_definition" "tech_challenge_client_task" {
-  family                   = "tech_challenge_client_task"
+resource "aws_ecs_task_definition" "tc_ms_client_cluster_task" {
+  family                   = "tc-ms-client-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
+  execution_role_arn       = aws_iam_user_policy_attachment.ec2_policy_attachment.policy_arn
+  cpu                      = "512" # 0.5 vCPU
+  memory                   = "1024" # 1GB
 
   container_definitions = jsonencode([
     {
-      name        : "tech-challenge-client-container",
-      image       : "jaircmendes/techchallenge:tcclient",
-      cpu         : 256,
-      memory      : 512,
-      essential   : true,
-      portMappings: [
+      name      = "ts-ms-client-container"
+      image     = "${aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/tc-ms-client:latest"
+      essential = true
+      portMappings = [
         {
-          containerPort: 3000,
-          hostPort     : 3000
+          containerPort = 80
+          hostPort      = 80
+          protocol      = "tcp"
+        },
+        {
+          containerPort = 8080
+          hostPort      = 8080
+          protocol      = "tcp"
         }
       ]
     }
   ])
 }
 
-# ECS Service
-resource "aws_ecs_service" "tech_challenge_service" {
-  name            = "tech-challenge-client-service"
-  cluster         = aws_ecs_cluster.tech_challenge_cluster.id
-  task_definition = aws_ecs_task_definition.tech_challenge_client_task.arn
-  launch_type     = "FARGATE"
+# Criar a IAM Role para a Task Execution
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs_task_execution_role"
 
-  network_configuration {
-    subnets         = ["subnet-04715a6b7400f9757", "subnet-06f39bcc16f6bd4ce", "subnet-010e2a0f51a3d00bc"]
-    security_groups = ["sg-0edafd1e8b0f706e9"]
-    assign_public_ip = true
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Effect = "Allow"
+      },
+    ]
+  })
+}
+
+# 3. Criar Security Group para o cluster
+resource "aws_security_group" "tc_ms_client_sg" {
+  name        = "tc-ms-client-sg"
+  description = "Allow all inbound traffic"
+  vpc_id      = "vpc-025dcd22cfaa4c66a"
+
+  ingress {
+    description      = "Allow all inbound traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1" # All
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
-  lifecycle {
-    create_before_destroy = true
+  egress {
+    description      = "Allow all outbound traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1" # All
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 }
+
