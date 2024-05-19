b1c5a56b0a6b7fc95e85b9e2ca4acdd05172e210
jaircavalcante <jcavalcante49@gmail.com>
2024-05-19 09:01:11 -0300

    ajuste unit clientcontrollertest

--- src/test/java/br/com/fiap/ms/client/ClientControllerTest.java
+++ src/test/java/br/com/fiap/ms/client/ClientControllerTest.java
@@ -1,5 +1,6 @@
-package br.com.fiap.ms.client.adapter.controller;
+package br.com.fiap.ms.client;
 
+import br.com.fiap.ms.client.adapter.controller.ClientController;
 import br.com.fiap.ms.client.application.dto.ClientDto;
 import br.com.fiap.ms.client.domain.exception.InvalidProcessException;
 import br.com.fiap.ms.client.domain.exception.client.InvalidClientProcessException;
@@ -8,30 +9,32 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.zalando.problem.ThrowableProblem;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.zalando.problem.Problem;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ClientControllerTest {
 
     @Mock
     private ClientUseCaseInterface clientUseCaseInterface;
-    private ClientController subject;
+    private ClientController controller;
 
     @BeforeEach
     void setup() {
-        subject = new ClientController(clientUseCaseInterface);
+        controller = new ClientController(clientUseCaseInterface);
     }
 
     @Test
     void shouldRegister() throws InvalidClientProcessException {
         ClientDto clientDto = new ClientDto("123");
-        Mockito.when(clientUseCaseInterface.register(clientDto)).thenReturn(clientDto);
+        when(clientUseCaseInterface.register(clientDto)).thenReturn(clientDto);
 
-        var response = subject.register(clientDto);
+        var response = controller.register(clientDto);
         assertNotNull(response);
     }
 
@@ -39,26 +42,38 @@ class ClientControllerTest {
     void shouldEdit() {
         ClientDto clientDto = new ClientDto("123");
 
-        var response = subject.edit(clientDto);
+        var response = controller.edit(clientDto);
         assertNotNull(response);
     }
 
     @Test
     void shouldRemove() {
-        var response = subject.remove("11111111111");
+        var response = controller.remove("11111111111");
         assertNotNull(response);
     }
 
     @Test
     void shouldFindAll() {
-        var response = subject.findAll();
+        var response = controller.findAll();
         assertNotNull(response);
     }
 
     @Test
     void shouldFindByDocument() {
-        var response = subject.findByDocument("11111111111");
+        var response = controller.findByDocument("11111111111");
         assertNotNull(response);
     }
 
+    @Test
+    public void testEditClientWithValidData() {
+        // Given
+        ClientDto clientDto = new ClientDto("1", "John Doe", "123456789", "john@example.com", "1234567890");
+
+        // When
+        ResponseEntity<?> responseEntity = controller.edit(clientDto);
+
+        // Then
+        assertNotNull(responseEntity);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    }
 }
