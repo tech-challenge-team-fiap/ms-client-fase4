93457fddacceca3209958978e8a037a8072ff998
Fontgalland <44510192+fontgalland@users.noreply.github.com>
2024-05-18 22:28:33 -0300

    Update ClientUseCaseInterfaceTest.java

--- src/test/java/br/com/fiap/ms/client/ClientUseCaseInterfaceTest.java
+++ src/test/java/br/com/fiap/ms/client/ClientUseCaseInterfaceTest.java
@@ -7,6 +7,7 @@ import br.com.fiap.ms.client.adapter.gateways.ClientGatewayInterface;
 import br.com.fiap.ms.client.application.dto.ClientDto;
 import br.com.fiap.ms.client.application.usecases.ClientUseCaseImpl;
 import br.com.fiap.ms.client.domain.exception.InvalidProcessException;
+import br.com.fiap.ms.client.domain.exception.client.ClientNotFoundException;
 import br.com.fiap.ms.client.domain.exception.client.InvalidClientProcessException;
 import br.com.fiap.ms.client.domain.interfaces.ClientUseCaseInterface;
 import br.com.fiap.ms.client.external.infrastructure.gateway.ClientGatewayImpl;
@@ -18,6 +19,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -62,5 +64,50 @@ public class ClientUseCaseInterfaceTest {
         assertNotNull(actualClient);
         assertEquals(expectedClient, actualClient);
 //        verify(clientUseCaseInterface, times(1)).findByCpf(cpf);
+    }
+
+    @Test
+    void shouldFindByDocument() throws ClientNotFoundException {
+        String cpf = "12345678900";
+        ClientDto expectedClient = new ClientDto(); // Set properties as needed
+
+        when(clientUseCaseInterface.findByDocument(cpf)).thenReturn(expectedClient);
+
+        var response = clientUseCaseInterface.findByDocument(cpf);
+
+        assertNotNull(response);
+    }
+
+    @Test
+    void shouldEdit() throws InvalidClientProcessException {
+        ClientDto expectedClient = new ClientDto("Teste", "99999999999", "test@test.com", "+5511999999999", "10/10/2010"); // Set properties as needed
+        ClientDto expectedClient2 = new ClientDto("Teste", "99999999999", "test2@test.com", "+5511999999999", "10/10/2010"); // Set properties as needed
+
+
+        when(clientUseCaseInterface.edit(expectedClient)).thenReturn(expectedClient2);
+
+        var response = clientUseCaseInterface.edit(expectedClient);
+
+        assertNotNull(response);
+    }
+
+    @Test
+    void shouldRemove() throws InvalidClientProcessException {
+        ClientDto expectedClient = new ClientDto("Teste", "99999999999", "test@test.com", "+5511999999999", "10/10/2010"); // Set properties as needed
+        
+        when(clientUseCaseInterface.remove("99999999999")).thenReturn(expectedClient);
+
+        var response = clientUseCaseInterface.remove("99999999999");
+
+        assertNotNull(response);
+    }
+
+    @Test
+    void shouldRegister() throws InvalidClientProcessException {
+        ClientDto expectedClient = new ClientDto("Teste", "99999999999", "test@test.com", "+5511999999999", "10/10/2010"); // Set properties as needed
+
+        var response = clientUseCaseInterface.register(expectedClient);
+
+        assertNull(response);
     }
 }
